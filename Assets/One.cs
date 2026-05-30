35364f18c55262e7b987b929cc5173a96cd528e4
영2 <yuni534@gmail.com>
2026-05-30 14:26:25 +0900

    Update One.cs

--- Assets/One.cs
+++ Assets/One.cs
@@ -5,7 +5,9 @@ public class One : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // 안녕
+        // 안녕456456
+        float speed;
+        Debug.Log("Speed");
     }
 
     // Update is called once per frame
