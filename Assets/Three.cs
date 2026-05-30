e3ea16b9f6360e92048d5b1445b697584d145ac9
영2 <yuni534@gmail.com>
2026-05-30 14:20:03 +0900

    Update Three.cs

--- Assets/Three.cs
+++ Assets/Three.cs
@@ -5,7 +5,7 @@ public class Three : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       // �ѱ��׽�Ʈ
+       // ÇÑ±ÛÅ×½ºÆ®123
     }
 
     // Update is called once per frame
