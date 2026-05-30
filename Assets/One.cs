84540232f939021fac0a27e829cb15e69f7595dc
영2 <yuni534@gmail.com>
2026-05-30 14:27:18 +0900

    A커밋
    A커밋

--- Assets/One.cs
+++ Assets/One.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class One : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // 안녕
+        // 안녕123123
+        int damage = 10;
+        Debug.Log("데미지 : " + damage);
     }
 
     // Update is called once per frame
