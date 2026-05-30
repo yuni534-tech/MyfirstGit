2abac1a81f1e2f2812d731e3ae90ccfe1ee941a7
영2 <yuni534@gmail.com>
2026-05-30 14:12:45 +0900

    초기 커밋
    초기 커밋

---
+++ Assets/One.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class One : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 안녕
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Three.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Three : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       // 한글테스트
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
